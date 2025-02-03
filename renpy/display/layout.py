f0ec388627e4ebf51b11a3a9f0760e76aca8c178
Davila Yoimer <89022117+yoimerdr@users.noreply.github.com>
2025-02-03 08:42:34 -0500

    ✨ feat: add new dummy functions and classes

--- renpy/display/layout.py
+++ renpy/display/layout.py
@@ -3,3 +3,8 @@ from renpy.display.displayable import Displayable
 
 class Null(Displayable):
     pass
+
+
+def Composite(size, positions, displayable, *args, **kwargs):
+    from renpy.display.im import Composite
+    return Composite(size, positions, displayable)
---
+++ store/__init__.py
@@ -0,0 +1,3 @@
+class Return:
+    def __call__(self, *args, **kwargs):
+        pass
