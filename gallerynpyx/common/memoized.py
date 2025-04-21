5444c72e6399e79256cdb85811fb14955dfe4cb1
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2025-04-21 10:24:09 -0500

    ✨ feat: add class wrapper for cache computed values

---
+++ gallerynpyx/common/memoized.py
@@ -0,0 +1,23 @@
+class Memoized(object):
+    __slots__ = (
+        '_res', '_fn',
+        '_val',
+    )
+
+    def __init__(self, builder):
+        self._fn = builder
+        self._val = None
+
+    def dispose(self):
+        self._val = None
+        try:
+            delattr(self, '_res')
+        except AttributeError:
+            pass
+
+    def evaluate(self, *args):
+        if self._val != args or not hasattr(self, '_res'):
+            self._val = args
+            setattr(self, '_res', self._fn(*args))
+
+        return self._res
