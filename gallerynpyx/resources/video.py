7976d2ccfafea1d0e9f6b84d71483336f55bd80d
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2025-04-26 11:02:01 -0500

    🦄 refactor: resource initialization and displayable creation.

--- gallerynpyx/resources/video.py
+++ gallerynpyx/resources/video.py
@@ -2,6 +2,7 @@ import os
 
 from renpy.display.video import Movie
 from .resource import Resource, VIDEOS
+from ..common.helpers import isdefine
 from ..common.compat import basestring
 from ..common.memoized import Memoized
 from ..path import isloadable, normpath
@@ -32,11 +33,12 @@ class VideoResource(Resource):
         return isinstance(resource, VideoResource)
 
     def _init_from_raw(self, source):
-        ext = None
         if isinstance(source, basestring):
             source = normpath(source)
-            ext = os.path.splitext(source)[1] or None
-        self._ext = ext
+            self._ext = os.path.splitext(source)[1] or None
+        elif isdefine(source):
+            self._ext = None
+            self._init_from_raw(getattr(source, '_original_play'))
         return super(VideoResource, self)._init_from_raw(source)
 
     def _load(self, force):
@@ -44,7 +46,7 @@ class VideoResource(Resource):
         return getattr(source, '_original_play', source)
 
     def _displayable(self, source):
-        return source if not self.ext else Movie(play=source)
+        return source if not isinstance(source, basestring) else Movie(play=source)
 
     def displayable(self, *args):
         return self._dmem.evaluate(self.source)
