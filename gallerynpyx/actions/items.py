c8ee1fdd77d1cac0955bbe4904a10f1b3e541f6a
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2025-04-28 10:07:38 -0500

    🦄 refactor: item display

--- gallerynpyx/actions/items.py
+++ gallerynpyx/actions/items.py
@@ -1,19 +1,26 @@
 from renpy.config import screen_width, screen_height
-from renpy.exports import music_start, movie_cutscene, show_screen, music_stop, invoke_in_new_context, transition
+from renpy.exports import music_start, show_screen, music_stop, invoke_in_new_context, transition
 from renpy.ui import Action, saybehavior, interact
-
 from ..anim import set_speed
 from ..common.classes.helpers import add_metaclass
 from ..common.classes.meta import RegistryMeta
+from ..common.exceptions import AssignmentError
 from ..config import ScreensConfig, ResourcesConfig
 from ..resources.displayable import DisplayableResource
-from ..resources.thumbnail import creates
 from ..resources.video import VideoResource
 from ..slides.items import isitem
-from ..common.helpers import isdefine
-from ..common.exceptions import AssignmentError
 
-__all__ = ('ShowItem',)
+__all__ = ('ShowItem', 'prepare_resource')
+
+
+def prepare_resource(resource):
+    cfg = ResourcesConfig.get_instance()
+    if isinstance(resource, (DisplayableResource, VideoResource)):
+        return resource.displayable((screen_width, screen_height))
+    res = resource.load(True)
+    if cfg.allow_animation_speeds:
+        set_speed(res, cfg.animation_speed)
+    return res
 
 
 @add_metaclass(RegistryMeta)
@@ -32,37 +39,41 @@ class ShowItem(Action):
         item = self._item
         return item.locked_tooltip if item.locked else item.tooltip
 
+    def _show_displayable(self, resource, ):
+        item = self._item
+        cfg = ResourcesConfig.get_instance()
+        saybehavior()
+        transition(cfg.transition)
+        res = prepare_resource(resource)
+        show_screen(ScreensConfig.get_instance().images_screen, res, item)
+        interact()
+        if not isinstance(resource, VideoResource):
+            transition(cfg.transition)
+        elif item.song:
+            music_stop()
+
     def show(self):
         item = self._item
         if item.locked:
-            return None
+            return
 
         song, resource = item.song, item.resource
         if song:
             music_start(song)
 
-        cfg = ResourcesConfig.get_instance()
         if isinstance(resource, VideoResource):
-            return movie_cutscene(resource.load(True), loops=-1, stop_music=isdefine(song))
+            self._show_movie(resource)
+            return
         else:
-            saybehavior()
-            transition(cfg.transition)
-            if isinstance(resource, DisplayableResource):
-                res = resource.displayable((screen_width, screen_height))
-            else:
-                res = resource.load(True)
-                if cfg.allow_animation_speeds:
-                    set_speed(res, cfg.animation_speed)
+            self._show_displayable(resource)
 
-            show_screen(ScreensConfig.get_instance().images_screen, res, item)
         if song:
-            music_stop(song)
-        interact()
-        transition(cfg.transition)
-        return None
+            music_stop()
 
     def __call__(self, *args, **kwargs):
         if self._item.locked:
             return
 
         invoke_in_new_context(self.show)
+
+    _show_movie = _show_displayable
