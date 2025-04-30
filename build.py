1d4f283db6ee4d1d04176c349c6f411c619b29d6
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2025-04-30 13:28:43 -0500

    🔧 build: add path setup and file cleanup steps

--- build.py
+++ build.py
@@ -1,9 +1,26 @@
-from py2ren.cli import main, Args
+import pathlib
+import shutil
 from py2ren import create_config
+from py2ren.cli import main, Args
 
-path = "./gallerynpyx"
+# checking paths
+root = pathlib.Path(__file__).parent
+path = root.joinpath("gallerynpyx")
+out = root.joinpath("dist", path.name)
+
+shutil.rmtree(out, ignore_errors=True)
+
+# Init config
 cfg = create_config(path, level=-1, store_modules=["gallerynpyx"], analyze_dependencies=True)
 
+## Internal module name checking. Exists an unexpected behavior with underscore names
+internal = cfg.modules["_internal"]
+internal._name = "_internal"
+
+events = internal.modules["_events.py"]
+events._name = "_events"
+
+# Build
 main(
     Args(
         "./gallerynpyx",
@@ -11,3 +28,7 @@ main(
         config=cfg
     )
 )
+
+# copy files
+shutil.copytree(root.joinpath("images"), out.joinpath("images"), dirs_exist_ok=True)
+shutil.copytree(root.joinpath("scripts"), out.joinpath("scripts"), dirs_exist_ok=True)
