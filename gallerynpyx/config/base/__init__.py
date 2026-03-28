f4b6b39dcb7ad15708771223809ab0c979bdd108
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2026-03-27 19:52:42 -0500

    chore: initialize module

---
+++ gallerynpyx/config/base/__init__.py
@@ -0,0 +1,11 @@
+from .resources import *
+from .screens import *
+from .styles import *
+from .manager import *
+
+__all__ = (
+    'ResourcesConfig',
+    'ScreensConfig',
+    'StylesConfig',
+    "ConfigManager"
+)
