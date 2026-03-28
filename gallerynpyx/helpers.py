3b213e27514a54025bfa44e8e6b9ff40e6c84755
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2026-03-28 12:36:40 -0500

    fix: incorrect arg usage

--- gallerynpyx/helpers.py
+++ gallerynpyx/helpers.py
@@ -8,7 +8,7 @@ __all__ = ('create_buttons',)
 def create_buttons(items, size, resources_config=None, screens_config=None):
     for ((item, displayable), (idle, hover, locked)) in create_items_overlay(items, size, resources_config):
         yield Button(
-            child=item, selected_child=locked,
+            child=displayable, selected_child=locked,
             action=ShowItem(item, ),
             idle_foreground=idle, hover_foreground=hover,
             yalign=0.5, xalign=0.5,
