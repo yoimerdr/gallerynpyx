2b03f097df1816a7bced98136770f512199e5128
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2026-03-28 15:31:52 -0500

    chore: update release version

--- gallerynpyx/exports.py
+++ gallerynpyx/exports.py
@@ -4,4 +4,4 @@ from .slides.items import Item as create_item, isitem
 from .slides.helpers import slide as create_slide, slider as create_slider
 from .slides.base import isslide, isslider
 
-version = '1.0.0'
+version = '2.0.0'
